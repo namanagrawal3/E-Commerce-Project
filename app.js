fe50da476174e486d2a23de08308c8a412dd1390
namanagrawal3 <agrawalnaman359@gmail.com>
2025-07-03 21:29:50 +0530

    updated session time

--- app.js
+++ app.js
@@ -51,8 +51,8 @@ const configSession = {
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    expires: Date.now() + 7*24*60*60*1000,
-    maxAge: 7*24*60*60*1000
+    expires: Date.now() + 1*24*60*60*1000,
+    maxAge: 1*24*60*60*1000
   }
 };
 
